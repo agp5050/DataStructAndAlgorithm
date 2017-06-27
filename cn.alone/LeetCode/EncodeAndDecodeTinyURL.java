504a443db5690c1cfb8c96fbaf2400e102d855d7
RojerAlone <1091165843@qq.com>
2017-06-27 11:51:43 +0800

    Two Easy Problems.

---
+++ cn.alone/LeetCode/EncodeAndDecodeTinyURL.java
@@ -0,0 +1,45 @@
+package LeetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * Created by RojerAlone on 2017/6/27
+ * https://leetcode.com/problems/encode-and-decode-tinyurl/
+ * 将长URL和短URL在map中双向映射，生成短URL时随机选取6个字符，如果有重复的，就再次生成
+ */
+public class EncodeAndDecodeTinyURL {
+
+    private static final String CODE = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    private Map<String, String> map = new HashMap<>();
+
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        String res = map.get(longUrl);
+        if (res != null) {
+            return res;
+        }
+        res = "http://tinyurl.com/";
+        Random random = new Random();
+        for (int i = 0; i < 6; i++) {
+            res += CODE.charAt(random.nextInt(62));
+        }
+        // 有重复的就再次生成
+        while (map.containsKey(res)) {
+            for (int i = 0; i < 6; i++) {
+                res += CODE.charAt(random.nextInt(62));
+            }
+        }
+        map.put(longUrl, res);
+        map.put(res, longUrl);
+        return res;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return map.get(shortUrl);
+    }
+
+}
---
+++ cn.alone/LeetCode/HammingDistance.java
@@ -0,0 +1,25 @@
+package LeetCode;
+
+/**
+ * Created by RojerAlone on 2017/6/27.
+ * https://leetcode.com/problems/hamming-distance/
+ * 汉明距离，将两个数异或，求出异或后二进制中1的位数
+ */
+public class HammingDistance {
+
+    public int hammingDistance(int x, int y) {
+        int res = 0;
+        // 异或
+        int tmp = x ^ y;
+        // 每次将 tmp 和 tmp - 1 相与
+        // 如 0101 & 0100 = 0100
+        //    0100 & 0011 = 0000
+        // 相当于每次去除了一个1
+        while (tmp > 0) {
+            res++;
+            tmp = tmp & (tmp - 1);
+        }
+        return res;
+    }
+
+}
